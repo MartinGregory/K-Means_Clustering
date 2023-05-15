83e4ddebc487e20166574bc6761ae57ea196e808
MartinGregory <martin.gregory.sendrowicz@gmail.com>
2023-05-15 01:25:01 -0400

    Add K-Means Clustering Xcode project

---
+++ K-Means_Clustering/K-Means_Clustering/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  K-Means_Clustering
+//
+//  Created by Martin Gregory Sendrowicz on 5/15/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
